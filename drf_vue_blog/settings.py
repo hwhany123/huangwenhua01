47a2948ddfa267b7e7cfd74640527e031a713879
hwhany123 <253922804@qq.com>
2023-06-12 22:54:08 +0800

    2023061202

--- drf_vue_blog/settings.py
+++ drf_vue_blog/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':10000,
+    'PAGE_SIZE':20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
